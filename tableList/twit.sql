850fe8aec3f7b4d1713cdf9962a5bac9c622815b
Jeon <wk0012345@gmail.com>
2019-09-24 22:57:39 +0900

    add sql

--- tableList/twit.sql
+++ tableList/twit.sql
@@ -1,8 +1,8 @@
 create table twit(
 	twit_id serial,
-	twit_text character(300),
+	twit_text varchar(300),
 	twit_content varchar,
-	username character varying(150),
+	username character varying(150) not null,
 	up_time timestamp default now(),
 	primary key (twit_id),
 	foreign key (username) references auth_user (username)
